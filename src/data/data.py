499ad7b7ac5641c5407d5c122557bbc32d00542e
Karan Dixit <dixitk941@gmail.com>
2025-03-15 15:26:21 +0530

    chore: remove hardcoded Firebase service account path for security reasons

--- src/data/data.py
+++ src/data/data.py
@@ -3,7 +3,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Initialize Firebase Admin SDK
-cred = credentials.Certificate("C:/Users/dixit/OneDrive/Documents/GitHub/yupmovies/src/data/goforcab-941-2bbc38a80938.json")
+cred = credentials.Certificate("")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
